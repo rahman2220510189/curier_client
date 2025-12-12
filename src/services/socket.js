f046a2a4e0056ad77f0b0f3dd246202c65d7673f
rahman2220510189 <rahman22205101894@diu.edu.bd>
2025-12-12 20:24:18 +0600

    final deploy

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL =  'http://localhost:5000';
+const SOCKET_URL =  'https://curier-server.onrender.com/';
 
 class SocketService {
   constructor() {
