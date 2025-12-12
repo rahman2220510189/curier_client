a358830c7acd863c5379d01ae3304dd98d2b96ae
rahman2220510189 <rahman22205101894@diu.edu.bd>
2025-12-12 21:06:39 +0600

    r koto error

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL =  'https://curier-server.onrender.com';
+const SOCKET_URL =  'https://curier-server.onrender.com/';
 
 class SocketService {
   constructor() {
